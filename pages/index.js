e499168d1d6dd98a050edb1172f0cc7e73d54ffb
semight <davidoluwasemiloorei@gmail.com>
2022-11-16 23:08:43 +0100

    adding of favicon

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import favicon from "../assets/favicon.webp"
 
 const Home = () => {
   return (
@@ -10,6 +11,8 @@ const Home = () => {
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400&display=swap"
           rel="stylesheet"
         />
+        <link rel="shortcut icon" href={favicon.src} />
+
       </Head>
       <h1>HELLO Dev.</h1>
       <h1>Pick it up from here</h1>
