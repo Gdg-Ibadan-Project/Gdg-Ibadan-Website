8dbe4a800b366ed2ee650bd97b96f7ce30f0db0f
Holytech <akeemqudus2016@gmail.com>
2022-11-20 15:28:13 +0100

    Fixed glitches that were obstructing deployment

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -1,16 +1,17 @@
+import Image from "next/image";
 import Styles from "./Hero.module.scss";
+import hero from "../../public/hero-icon.svg";
 
 const Hero = () => {
-    return (
-        <div className={Styles.container}>
-            <p>Welcome to</p>
-            <section>
-                <h1>GDG IBADAN ANNUAL FESTIVAL 2022</h1>
-                <img src="/hero-icon.svg" />
-            </section>
-
-        </div>
-    )
-}
+  return (
+    <div className={Styles.container}>
+      <p>Welcome to</p>
+      <section>
+        <h1>GDG IBADAN ANNUAL FESTIVAL 2022</h1>
+        <Image src={hero} alt="hero's icon" />
+      </section>
+    </div>
+  );
+};
 
 export default Hero;
