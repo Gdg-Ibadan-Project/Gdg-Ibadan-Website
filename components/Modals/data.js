d2f3e854a7989945c86c81c431169a17e813c95f
abdulroheem <ayanwunmiabdulroheem@gmail.com>
2022-11-20 23:10:04 +0100

    testing

--- components/Modals/data.js
+++ components/Modals/data.js
@@ -34,5 +34,16 @@ export const data = [
         twitter: 'https://facebook.com/tunde',
         insta: 'https://facebook.com/tunde',
         linkedIn: 'https://facebook.com/tunde',
+    },
+    {
+        id: 4,
+        image:  image,
+        title: 'Icitify Company Limited',
+        level: 'Silver Sponsor',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt et ipsam animi ut nostrum iure nemo vel soluta laborum odit fugiat mollitia, eius possimus veniam facere harum accusamus exercitationem voluptatibus dolorum dolores ipsum temporibus architecto eveniet? Labore possimus voluptate nisi alias aspernatur explicabo autem ut non odio quisquam sapiente debitis praesentium ipsum consequuntur culpa impedit voluptatibus ratione nostrum, quos repellat!',
+        facebook: 'https://facebook.com/tunde',
+        twitter: 'https://facebook.com/tunde',
+        insta: 'https://facebook.com/tunde',
+        linkedIn: 'https://facebook.com/tunde',
     }
 ]
