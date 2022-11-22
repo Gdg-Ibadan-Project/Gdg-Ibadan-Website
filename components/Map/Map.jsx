e1e41ba0b22a53ee2d5f56302425247b7e1d0d8f
Holytech <akeemqudus2016@gmail.com>
2022-11-22 02:50:27 +0100

    hide the api key

--- components/Map/Map.jsx
+++ components/Map/Map.jsx
@@ -4,7 +4,7 @@ import styles from "./Map.module.scss";
 
 const Map = () => {
   const { isLoaded } = useLoadScript({
-    googleMapsApiKey: "AIzaSyAxTrGqIHWJ9D3I6BiL2wHvl6qzE3VNk8w",
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
   });
 
   const MapComponent = () => {
